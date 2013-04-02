75bcfdb21f16149b58405475221ce21f10768ca4
jubianchi <contact@jubianchi.fr>
2013-04-02 20:12:31 +0200

    Improve file (re)loading

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 var atoum = module.exports = require('./lib/atoum.js');
+
+atoum.require = function(module, from) {
+    var modulepath;
+
+    if(typeof from !== 'undefined') {
+        var basedir = require('path').dirname(module.filename);
+        modulepath = arguments[0] = require('path').resolve(basedir, arguments[0]);
+    } else {
+        modulepath = require.resolve(arguments[0]);
+    }
+
+    if(require.cache[modulepath]) {
+        delete require.cache[modulepath];
+    }
+
+    return require.apply(require, arguments);
+};
