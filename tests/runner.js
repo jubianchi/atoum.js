b43d201907e4a2ecc65b16a4a50cbc33923a9552
jubianchi <contact@jubianchi.fr>
2013-04-08 00:52:57 +0200

    Improve runner test

--- tests/runner.js
+++ tests/runner.js
@@ -1,4 +1,5 @@
 var atoum = require('..')(module),
+    callback = require('../lib/test/callback'),
     testedClass = require('../lib/runner'),
     unit = module.exports = {
         testClass: function() {
@@ -15,18 +16,21 @@ var atoum = require('..')(module),
         },
 
         testSetLoop: function() {
-            var report, generator, object;
+            var report, generator, object, readline, interface;
 
             this
                 .if(generator = {})
-                .and(report = { register: function() { return this; } })
+                .and(report = {})
+                .and(readline = { createInterface: callback(function() { return (interface = { close: callback() }); }) })
                 .then()
                     .object(object = new testedClass(report, generator))
                     .bool(object.loop).isFalse()
-                    .object(object.setLoop(true)).isIdenticalTo(object)
+                    .object(object.setLoop(true, readline)).isIdenticalTo(object)
                     .bool(object.loop).isTrue()
+                    .callback(readline.createInterface).wasCalled()
                     .object(object.setLoop(false)).isIdenticalTo(object)
                     .bool(object.loop).isFalse()
+                    .callback(interface.close).wasCalled().withoutArgument()
             ;
         }
     };
