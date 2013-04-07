6a8a7c14c6bb5d9764dd55371ce524da2d74ff55
jubianchi <contact@jubianchi.fr>
2013-04-08 00:17:12 +0200

    Indentation

--- tests/runner.js
+++ tests/runner.js
@@ -1,32 +1,32 @@
 var atoum = require('..')(module),
     testedClass = require('../lib/runner'),
     unit = module.exports = {
-    testClass: function() {
-        var report, generator, object;
+        testClass: function() {
+            var report, generator, object;
 
-        this
-            .if(generator = {})
-            .and(report = { register: function() { return this; } })
-            .then()
-                .object(object = new testedClass(generator))
-                .array(object.reports).isEmpty()
-                .object(object.generator).isIdenticalTo(generator)
-        ;
-    },
+            this
+                .if(generator = {})
+                .and(report = { register: function() { return this; } })
+                .then()
+                    .object(object = new testedClass(generator))
+                    .array(object.reports).isEmpty()
+                    .object(object.generator).isIdenticalTo(generator)
+            ;
+        },
 
-    testSetLoop: function() {
-        var report, generator, object;
+        testSetLoop: function() {
+            var report, generator, object;
 
-        this
-            .if(generator = {})
-            .and(report = { register: function() { return this; } })
-            .then()
-                .object(object = new testedClass(report, generator))
-                .bool(object.loop).isFalse()
-                .object(object.setLoop(true)).isIdenticalTo(object)
-                .bool(object.loop).isTrue()
-                .object(object.setLoop(false)).isIdenticalTo(object)
-                .bool(object.loop).isFalse()
-        ;
-    }
-};
+            this
+                .if(generator = {})
+                .and(report = { register: function() { return this; } })
+                .then()
+                    .object(object = new testedClass(report, generator))
+                    .bool(object.loop).isFalse()
+                    .object(object.setLoop(true)).isIdenticalTo(object)
+                    .bool(object.loop).isTrue()
+                    .object(object.setLoop(false)).isIdenticalTo(object)
+                    .bool(object.loop).isFalse()
+            ;
+        }
+    };
