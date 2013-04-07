c666cfb02c220d7462489df6d4a2288e6735f35d
jubianchi <contact@jubianchi.fr>
2013-04-07 17:15:06 +0200

    Add light cli report

--- tests/report/fields/runner/node.js
+++ tests/report/fields/runner/node.js
@@ -2,7 +2,7 @@ var util = require('util'),
     color = require('cli-color'),
     atoum = require('../../../../')(module),
     field = require('../../../../lib/report/field'),
-    testedClass = require('../../../../lib/report/fields/runner/atoum'),
+    testedClass = require('../../../../lib/report/fields/runner/node'),
     unit = module.exports = {
         testClass: function() {
             var object;
@@ -19,7 +19,13 @@ var util = require('util'),
             this
                 .if(object = new testedClass())
                 .then()
-                    .string(object.toString()).isEqualTo(util.format(color.blue('atoum.js %s\n\n'), atoum.version))
+                    .string(object.toString()).isEqualTo(
+                        util.format('> ' + color.bold('node path') + ': %s\n', process.execPath)
+                            .concat(util.format(
+                                '> ' + color.bold('node versions') + ': %s\n',
+                                util.inspect(process.versions)
+                            ))
+                    )
             ;
         }
     };
