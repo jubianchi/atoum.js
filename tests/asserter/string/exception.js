05499cb169a8f482243dec8eae3271178063a304
Julien Bianchi <julien.bianchi@pmsipilot.com>
2013-05-16 17:57:36 +0200

    New string diffs implementation

--- tests/asserter/string/exception.js
+++ tests/asserter/string/exception.js
@@ -24,41 +24,37 @@ var atoum = require('../../..')(module),
         },
 
         testTostring: function() {
-            var object, message, asserter, reference, data;
-
             this
                 .if(message = Math.random().toString(36).substring(7))
-                .and(reference = Math.random().toString(36).substring(7))
-                .and(data = Math.random().toString(36).substring(7))
                 .and(asserter = {})
+                .and(reference = 'bar\nfoo')
+                .and(data = 'foo\nbar\nbar')
                 .and(object = new testedClass(message, asserter, reference, data))
                 .then()
-                    .string(object.toString())
-                        .contains('Strings are not equal\n')
-                        .contains((function() {
-                            var diffcontent = '';
-
-                            underscore.each(
-                                diff.diffChars(data, reference),
-                                function(value) {
-                                    switch(true) {
-                                        case value.added:
-                                            diffcontent = diffcontent.concat(color.bgRed.white(value.value));
-                                            break;
-
-                                        case value.removed:
-                                            diffcontent = diffcontent.concat(color.bgGreen.white(value.value));
-                                            break;
-
-                                        default:
-                                            diffcontent = diffcontent.concat(value.value);
-                                            break;
-                                    }
-                                }
-                            );
-
-                            return diffcontent;
-                        }()))
+                .string(object.toString()).isEqualTo(
+                    "Strings are not equal\n"
+                        .concat(color.bgRed.white(" - Reference ")).concat(" ").concat(color.bgGreen.white(" + Data "))
+                        .concat("\n\n")
+                        .concat(color.bgXterm(238).white(" R  D ")).concat("\n")
+                        .concat(color.bgXterm(238).white("    1 ")).concat(" ").concat(color.bgGreen.white("foo")).concat("\n")
+                        .concat(color.bgXterm(238).white(" 1  2 ")).concat(" bar").concat("\n")
+                        .concat(color.bgXterm(238).white(" 2    ")).concat(" ").concat(color.bgRed.white("foo")).concat("\n")
+                        .concat(color.bgXterm(238).white("    3 ")).concat(" ").concat(color.bgGreen.white("bar")).concat("\n")
+                )
+                .if(reference = Math.random().toString(36).substring(7))
+                .and(data = Math.random().toString(36).substring(7))
+                .and(object = new testedClass(message, asserter, reference, data))
+                .then()
+                .string(object.toString()).isEqualTo(
+                    "Strings are not equal\n"
+                        .concat(color.bgRed.white(" - Reference ")).concat(" ").concat(color.bgGreen.white(" + Data "))
+                        .concat("\n\n")
+                        .concat(color.bgXterm(238).white(" R  D ")).concat("\n")
+                        .concat(color.bgXterm(238).white(" 1    ")).concat(" ").concat(color.bgRed.white(reference)).concat("\n")
+                        .concat(color.bgXterm(238).white("    1 ")).concat(" ").concat(color.bgGreen.white(data)).concat("\n")
+                )
             ;
+
+            var object, message, asserter, reference, data;
         }
     };
