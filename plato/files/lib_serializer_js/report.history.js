84d01e74108c5bcf359bec621778ad2a0e4766a7
jubianchi <contact@jubianchi.fr>
2013-10-18 23:37:51 +0200

    Update documentation

--- plato/files/lib_serializer_js/report.history.js
+++ plato/files/lib_serializer_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Fri, 26 Jul 2013 22:07:12 GMT","sloc":37,"lloc":19,"functions":4,"deliveredBugs":0.1720756750680247,"maintainability":75.55585494677335,"lintErrors":2,"difficulty":15.3}]
+__history = [{"date":"Fri, 26 Jul 2013 22:07:12 GMT","sloc":37,"lloc":19,"functions":4,"deliveredBugs":0.1720756750680247,"maintainability":75.55585494677335,"lintErrors":2,"difficulty":15.3},{"date":"Fri, 18 Oct 2013 21:37:50 GMT","sloc":37,"lloc":19,"functions":4,"deliveredBugs":0.1720756750680247,"maintainability":75.55585494677335,"lintErrors":2,"difficulty":15.3}]
